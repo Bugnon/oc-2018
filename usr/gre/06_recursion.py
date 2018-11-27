7dee662df6c0c3f73b20066eaf6ced97fdddb10d
MichARom <mg.greub@icloud.com>
2018-11-27 08:27:22 +0100

    add files and modify in gre folder

---
+++ usr/gre/06_recursion.py
@@ -0,0 +1,14 @@
+# File: 06_recursion.py
+# Author: Michael Greub
+# Date: 27.11.2018
+
+# Saisi au clavier
+
+name=input('votre nom: ')
+print('\n Hello ', name)
+
+a=input('Un nombre : ')
+a=float(a)
+
+print(a)
+print(a*2)
