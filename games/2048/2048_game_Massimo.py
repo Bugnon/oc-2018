26417568aa5ca1215aa15cfcfa4a5430c5add897
Massimo Stefani <massimo.cruz25@gmail.com>
2019-01-17 10:54:52 +0100

    jkujo

--- games/2048/2048_game_Massimo.py
+++ games/2048/2048_game_Massimo.py
@@ -5,95 +5,187 @@
 # Importation des modules requis
 from sense_hat import SenseHat
 from random import randint
+from time import *
+from threading import Thread
 
 sense=SenseHat()
-sense.clear()
-z= 0
+sense.clear(0, 0, 0)
+size = 4
 
-#-----Définition des fonctions-----
+
+#-----Définition des couleurs-----
 
 black = (0, 0, 0)
-blue = (0, 0, 255)
-green = (0, 255, 0)
+blue_1 = (0, 255, 255)
+green_2 = (0, 255, 127)
+green_3 = (0, 255, 0)
+green_4 = (127, 255, 0)
+yellow_5 = (255, 255, 0)
+orange_6 = (255, 127, 0)
+red_7 = (255, 0, 0)
+pink_8 = (255, 0, 127)
+pink_9 = (255, 0, 255)
+pink_10 = (127, 0, 255)
+blue_11 = (0, 0, 255)
+blue_12 = (0, 127, 255)
+white_13 = (255, 255, 255)
 
-colors = [black, blue, green]
+colors = [black, blue_1, green_2, green_3, green_4, yellow_5, orange_6, red_7,\
+          pink_8, pink_9, pink_10, blue_11, blue_12, white_13]
 
-colors[1]
+# ------Définition des matrices utilisées------
 
-L = [[0, 0, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0],
-     [0, 0, 0, 0]
+
+L4 = [[0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0], 
+     ]
+L8 = [[0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
      ]
 
-L[0][0] = 1
+#----- Définitionts des fonctions-----
 
-print(L)
+def startup():
+    sense.clear()
+    sense.show_message('Choose your mode:',0.075)
+    sleep(0.2)
+    
 
-def collision(pixel):# Définition redondante de la fonction permettant de monter en couleur (chiffre)
-    pixelup = (0, 0, 0)
-    if pixel == (0, 252, 248): # 2
-        pixelup = (0, 252, 124) # 4
-    elif pixel == (0, 252, 124): # 4
-        pixelup = (0, 252, 0) # 8
-    elif pixel == (0, 252, 0): # 8
-        pixelup = (124, 252, 0) # 16
-    elif pixel == (124, 252, 0): # 16
-        pixelup = (248, 252, 0) # 32
-    elif pixel == (248, 252, 0): # 32
-        pixelup = (248, 126, 0) # 64
-    elif pixel == (248, 126, 0): # 64
-        pixelup = (248, 0, 0) # 128
-    elif pixel == (248, 0, 0): # 128
-        pixelup = (248, 0, 124) # 256
-    elif pixel == (248, 0, 124): # 256
-        pixelup = (248, 0, 248) # 512
-    elif pixel == (248, 0, 248): # 512
-        pixelup = (124, 0, 248) # 1024
-    elif pixel == (124, 0, 248): # 1024
-        pixelup = (0, 0, 248) # 2048
-    elif pixel == (0, 0, 248): # 2048
-        pixelup = (0, 126, 248) # 4096
-    elif pixel == (0, 126, 248): # 4096
-        pixelup = (248, 252, 248) # 8192
-    elif pixel == (248, 252, 248): # 8192
-        victory
-    return pixelup
-    
-    
-    
+def set_pixels(a):
+    if a==8:
+        for x in range(8):
+           for y in range(8):
+              sense.set_pixel(x, y, colors[L[x][y]])
+   elif a==4:
+        
+
 def new_block():
-  i=0
-  while i < 2: #tant qu'on en a pas créé 2
-    x=randint(0, 7)# 
-    y=randint(0, 7)# On choisis aléatoirement une ligne et une colonne
-    if sense.get_pixel(x, y) == [0, 0, 0]:# On controle si ce pixel est vide
-      start = (0, 255, 248)
-      sense.set_pixel(x, y, start) # On défini un bloc de couleur correspondant au chiffre 2
-      i=i+1 # Si le bloc est créé on indente pour créé exactement 2 nouveaux pixels
+    i=0
+    for x in range(4):
+        for y in range(4):
+            if L[x][y]==0:
+                i=i+1
+    if i>1:
+        r=0
+        while r<2: #tant qu'on en a pas créé 2
+            x=randint(0, 3)# 
+            y=randint(0, 3)
+            print(x,y)# On choisis aléatoirement une ligne et une colonne
+            if L[x][y] == 0:# On controle si ce pixel est vide
+                L[x][y]=1 # On défini un bloc de couleur correspondant au chiffre 2
+                r=r+1# Si le bloc est créé on indente pour créé exactement 2 nouveaux pixels
+    elif i==1:
+        r=0
+        while r < 1: #tant qu'on en a pas créé 2
+            x=randint(0, 3)# 
+            y=randint(0, 3)# On choisis aléatoirement une ligne et une colonne
+            if L[x][y] == 0:# On controle si ce pixel est vide
+                L[x][y]=1 # On défini un bloc de couleur correspondant au chiffre 2
+                r=r+1
+    set_pixels()
+    print(L)
 
 def moved_up():
-  for y in range(8): 
-    for x in range(8): # Sur chaque pixel en prenantles pixels en ligne puis en colonne
-      if sense.get_pixel(x, y) != (0, 0, 0) and y > 1:# On controle que le pixel ne soit pas une case vide
-          global z
-          z = y
-          while sense.get_pixel(x, z-1) == [0, 0, 0] and z>1:# Si la case est vide 
-              pixel=sense.get_pixel(x, z)
-              sense.set_pixel(x, z-1, pixel)
-              sense.set_pixel(x, z, 0, 0, 0)
-              z=z-1
-          if y != 0 :
-              pixel_to_upgrade = sense.get_pixel(x, y-1)
-              pixel_upgraded = collision(pixel_to_upgrade)
-              sense.set_pixel(x, y, 0, 0, 0)
-              sense.set_pixel(x, y-1, pixel_upgraded)
+  for y in range(4): 
+    for x in range(4):# Sur chaque pixel en prenantles pixels en ligne puis en colonne
+        if L[x][y] > 0 and y>=1:# On controle que le pixel ne soit pas une case vide
+          while L[x][y-1] == 0 and y>=1:# Si la case est vide 
+              L[x][y-1]=L[x][y]
+              L[x][y]=0
+              y=y-1
+          if L[x][y-1]==L[x][y]:
+             L[x][y-1]=L[x][y-1]+1
+             L[x][y]=0
   new_block()
-def moved_down():
-    pass
   
+  
+def moved_down():
+    for z in range(3):
+        for x in range(4):
+            y=2-z
+            if L[x][y] > 0 and y<=2:# On controle que le pixel ne soit pas une case vide
+                while y<=2 and L[x][y+1] == 0:# Si la case est vide
+                   L[x][y+1]=L[x][y]
+                   L[x][y]=0
+                   y=y+1
+                if y<3:
+                    if L[x][y+1]==L[x][y]:
+                       L[x][y+1]=L[x][y+1]+1
+                       L[x][y]=0
+    new_block()
+    
+   
+def moved_left():
+    for x in range(4):
+        for y in range(4):
+            if L[x][y] > 0:# On controle que le pixel ne soit pas une case vide
+                while x>0 and L[x-1][y] == 0:# Si la case est vide 
+                    L[x-1][y]=L[x][y]
+                    L[x][y]=0
+                    x=x-1
+                if L[x-1][y]==L[x][y]:
+                   L[x-1][y]=L[x-1][y]+1
+                   L[x][y]=0
+    new_block()
 
+def moved_right():
+    for z in range(3):
+        x=2-z
+        for y in range(4):
+            if L[x][y] > 0:# On controle que le pixel ne soit pas une case vide
+                while x<3 and L[x+1][y] == 0:# Si la case est vide 
+                    L[x+1][y]=L[x][y]
+                    L[x][y]=0
+                    x=x+1
+                if L[x+1][y]==L[x][y]:
+                   L[x+1][y]=L[x+1][y]+1
+                   L[x][y]=0
+    new_block()
+            
+
+    
+#    
+#def new_block():
+#  i=0
+#  while i < 2: #tant qu'on en a pas créé 2
+#    x=randint(0, 7)# 
+#    y=randint(0, 7)# On choisis aléatoirement une ligne et une colonne
+#    if sense.get_pixel(x, y) == [0, 0, 0]:# On controle si ce pixel est vide
+#      start = (0, 255, 248)
+#      sense.set_pixel(x, y, start) # On défini un bloc de couleur correspondant au chiffre 2
+#      i=i+1 # Si le bloc est créé on indente pour créé exactement 2 nouveaux pixels
+#
+#def moved_up():
+#  for y in range(8): 
+#    for x in range(8): # Sur chaque pixel en prenantles pixels en ligne puis en colonne
+#      if sense.get_pixel(x, y) != (0, 0, 0) and y > 1:# On controle que le pixel ne soit pas une case vide
+#          global z
+#          z = y
+#          while sense.get_pixel(x, z-1) == [0, 0, 0] and z>1:# Si la case est vide 
+#              pixel=sense.get_pixel(x, z)
+#              sense.set_pixel(x, z-1, pixel)
+#              sense.set_pixel(x, z, 0, 0, 0)
+#              z=z-1
+#          if y != 0 :
+#              pixel_to_upgrade = sense.get_pixel(x, y-1)
+#              pixel_upgraded = collision(pixel_to_upgrade)
+#              sense.set_pixel(x, y, 0, 0, 0)
+#              sense.set_pixel(x, y-1, pixel_upgraded)
+#  new_block()
+#def moved_down():
+#    pass
+#  
+#
 new_block()
+
 #-----Reactions du joystick-----
 running= True
 
@@ -110,3 +202,4 @@ while running:
               moved_left()
           elif event.direction == 'middle':
               pass
+
