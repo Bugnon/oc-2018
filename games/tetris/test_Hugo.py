e605c9cc29e49a7cea831cc64dd50b4272791073
Hugo Ducommun <hugo.ducommun@bluewin.ch>
2019-01-17 14:20:02 +0100

    Add matrix tests

--- games/tetris/test_Hugo.py
+++ games/tetris/test_Hugo.py
@@ -27,51 +27,36 @@ def O (x, y):
     sense.set_pixel(x-1, y-1, yellow)
 
 ## La barre en cyan
-def I (x, y):
+def I1 (x, y):
+    sense.clear()
     sense.set_pixel(x, y, cyan)
     sense.set_pixel(x-1, y, cyan)
     sense.set_pixel(x-2, y, cyan)
+def I2 (x, y):
+    sense.clear()
+    sense.set_pixel(x, y, cyan)
+    sense.set_pixel(x-1, y, cyan)
+    sense.set_pixel(x+1, y, cyan)
 
 ## Le T en violet
-def T (x, y):
+def T1 (x, y):
+    sense.clear()
     sense.set_pixel(x, y, magenta)
     sense.set_pixel(x-1, y, magenta)
     sense.set_pixel(x, y-1, magenta)
-
-## Le L en orange
-def L (x, y):
-    sense.set_pixel(x, y, orange)
-    sense.set_pixel(x, y-1, orange)
-    sense.set_pixel(x, y-2, orange)
-    sense.set_pixel(x-1, y-2, orange)
+def T2 (x, y):
+    sense.clear()
+    sense.set_pixel(x-1, y-1, magenta)
+    sense.set_pixel(x-1, y, magenta)
+    sense.set_pixel(x, y-1, magenta)
+def T3 (x, y):
+    sense.clear()
+    sense.set_pixel(x, y, magenta)
+    sense.set_pixel(x-1, y, magenta)
+    sense.set_pixel(x-1, y-1, magenta)
     
-## Le J en bleu
-def J (x, y):
-    sense.set_pixel(x, y, blue)
-    sense.set_pixel(x-1, y, blue)
-    sense.set_pixel(x-2, y, blue)
-    sense.set_pixel(x-2, y-1, blue)
-
-## Le Z en rouge
-def Z (x, y):
-    sense.set_pixel(x, y, red)
-    sense.set_pixel(x-1, y, red)
-    sense.set_pixel(x-1, y-1, red)
-    sense.set_pixel(x-2, y-1, red)
-
-## Le S en vert
-def S (x, y):
-    sense.set_pixel(x, y, green)
-    sense.set_pixel(x-1, y, green)
-    sense.set_pixel(x, y-1, green)
-    sense.set_pixel(x+1, y-1, green)
-    
-sense.clear()
-
-shapes = [S(4, 7), Z(4, 7), J(4, 7), L(4, 7), T(4, 7), O(4, 7)]
-functions = [S, Z, J, L, T, O]
-
-choice(shapes)
+##shapes = [S(4, 7), Z(4, 7), J(4, 7), L(4, 7), T1(4, 7), O(4, 7)]
+##functions = [S, Z, J, L, T1, O]
 
 ### JOYSTICK ###
     
