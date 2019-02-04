833e3209faaffe2c9c1c8b1e3d978088ee34ebad
Hugo Ducommun <hugo.ducommunditverron@bugnon.educanet2.ch>
2019-02-04 22:00:08 +0100

    aarf

--- games/mines/mines.py
+++ games/mines/mines.py
@@ -1,264 +1,169 @@
-# File: Démineur
-# Author: Fabian Roulin et Mirko Pirona
-# Date: 8.01.2019
+#Author: Mirko Pirona
+##Mines
+##Date
 
-# Il manque :nimation explode, win, lose, fontion qui compte de nombre de bombs around
+import random
 
-# On importe le module Sensehat
-from sense_hat import SenseHat
+class Cell(object):
+    def __init__(self, is_mine, is_visible=False, is_flagged=False):
+        self.is_mine = is_mine
+        self.is_visible = is_visible
+        self.is_flagged = is_flagged
 
-# On importe la librairie pygame
-import pygame
-from pygame.locals import *
+    def show(self):
+        self.is_visible = True
 
-# On importe le module random
-from random import randint, choice
+    def flag(self):
+        self.is_flagged = not self.is_flagged
 
-#On importe le module time
-from time import sleep, time
-
-#On définit nos couleurs :
-
-green = (0, 255, 0)       #Drapeau
-grey = (90,94,107)        #Case non-découverte
-white = (255, 255, 255)   #Case vide = 0 bombe
-yellow = (247, 255, 60)   #1 bombe à proximité
-orange = (255, 127, 0)    #2 bombes à proximité
-corail = (231, 62, 1)     #3 bombes à proximité
-red = (248, 0, 0)         #4 bombes à proximité
-bordeau = (91, 60, 19)    #5 bombe à proximité
-black = (0, 0, 0)         #Bombe
-
-# On lance pygame :
-pygame.init()
-pygame.display.set_mode((90,5)) # Correspond à une petite fenêtre
-
-# On lance sense hat :
-sense = SenseHat()
-sense.clear()
-
-# On initialise les variables globals:
-
-# On crée la variable qui contiendra les bombes :
-List_bomb = []
-
-# On crée les variables de déplacement :
-game = True
-running = True
-x = 0
-y = 0
-
-old_color = grey
-
-# Définition des fonctions du programme :
-
-# Fonction qui compte le nombre de bombes à proximité :
-def create_map(bombs):
-    T = [[0] *10] * 10
-    print(bombs)
-    for (x, y) in bombs:
-        print(x, y)
-        T[x][y] += 1
-        T[x+1][y] += 1
-        T[x][y+1] += 1
-        T[x+2][y] += 1
-        T[x][y+2] += 1
-        T[x+2][y+2] += 1
-        T[x+1][y+2] += 1
-        T[x+2][y+1] += 1
-        
-    
-    print(T)
+    def place_mine(self):
+        self.is_mine = True
 
 
-def count_bombs_around(x,y):
-    
-    global List_bomb
-    
-    nb = 0
-    
-    xy = x*10 + y
-    
-    xy += 10
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy += 1
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy -= 10
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy -= 10
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy -= 1
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy -= 1
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy += 10
-    
-    if xy in List_bomb:
-        nb += 1
-        
-    xy += 10
-    
-    if xy in List_bomb:
-        nb += 1
-    
-    return nb;
+class Board(tuple):
+    def __init__(self, tup):
+        super().__init__()
+        self.is_playing = True
 
-# Fonction qui choisi les bombes :
-def choose_bomb(nb_bomb):
-  
-  sense.clear()
-  
-  liste = []
-    
-  i=0
-  while i < nb_bomb :
-    x = randint(0, 7)
-    y = randint(0, 7)
-    if (x, y) not in liste:
-        liste.append((x, y))
-        i = i + 1
+    def __str__(self):
+        board_string = ("Mines: " + str(self.remaining_mines) + "\n  " +
+                        "".join([str(i) for i in range(len(self))]))
+        for (row_id, row) in enumerate(self):
+            board_string += "\n" + str(row_id) + " "
+            for (col_id, cell) in enumerate(row):
+                if cell.is_visible:
+                    if cell.is_mine:
+                        board_string += "M"
+                    else:
+                        board_string += str(self.count_surrounding(row_id,
+                                                                   col_id))
+                elif cell.is_flagged:
+                        board_string += "F"
+                else:
+                    board_string += "X"
+            board_string += " " + str(row_id)
+        board_string += "\n  " + "".join([str(i) for i in range(len(self))])
+        return board_string
 
-  return liste;
+    def show(self, row_id, col_id):
+        if not self[row_id][col_id].is_visible:
+            self[row_id][col_id].show()
 
-# Initialisation du plateau de jeu :
-def set_all_in_gray():
-    G = grey
+            if (self[row_id][col_id].is_mine and not
+                self[row_id][col_id].is_flagged):
+                self.is_playing = False
 
-    set_color_grey = [
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G,
-    G, G, G, G, G, G, G, G
-    ]
+            elif self.count_surrounding(row_id, col_id) == 0:
+                [self.show(surr_row, surr_col) for (surr_row, surr_col) in
+                 self.get_neighbours(row_id, col_id) if
+                 self.is_in_range(surr_row, surr_col)]
 
-    sense.set_pixels(set_color_grey)
+    def flag(self, row_id, col_id):
+        if not self[row_id][col_id].is_visible:
+            self[row_id][col_id].flag()
+        else:
+            print("Cannot add flag, cell already visible.")
 
-# Fonction pour une nouvelle partie :
-def new_game(nb):
-    
-    # On séléctionne la variable externe qui contiendra les bombes
-    global List_bomb
-    
-    # Création aléatoire des bombes
-    List_bomb = choose_bomb(nb)
-    
-    print(List_bomb)
-    create_map(List_bomb)
-    
-    # On réinitialise le plateau de jeu.
-    set_all_in_gray()
-    
+    def place_mine(self, row_id, col_id):
+        self[row_id][col_id].place_mine()
 
-# Initialisation des variables de déplacement.
-    global x
-    global y
-    
-    x = 0
-    y = 0
+    def count_surrounding(self, row_id, col_id):
+        count = 0
+        for (surr_row, surr_col) in self.get_neighbours(row_id, col_id):
+            if (self.is_in_range(surr_row, surr_col) and
+                self[surr_row][surr_col].is_mine):
+                count += 1
+        return count
 
-# Fonction qui effectue l'action sur la case selectionnée :
-def use_case(x, y):
+    def get_neighbours(self, row_id, col_id):
+        SURROUNDING = ((-1, -1), (-1,  0), (-1,  1),
+                       (0 , -1),           (0 ,  1),
+                       (1 , -1), (1 ,  0), (1 ,  1))
+        neighbours = []
+        for (surr_row, surr_col) in SURROUNDING:
+            neighbours.append((row_id + surr_row, col_id + surr_col))
+        return neighbours
 
-    global List_bomb
+    def is_in_range(self, row_id, col_id):
+        return 0 <= row_id < len(self) and 0 <= col_id < len(self)
 
-    if (x, y) in List_bomb:
-        sense.clear(255,0,0)
-        #explode(x,y) # Fonction à créer
+    @property
+    def remaining_mines(self):
+        remaining = 0
+        for row in self:
+            for cell in row:
+                if cell.is_mine:
+                    remaining += 1
+                if cell.is_flagged:
+                    remaining -= 1
+        return remaining
+
+    @property
+    def is_solved(self):
+        for row in self:
+            for cell in row:
+                if not(cell.is_visible or cell.is_flagged):
+                    return False
+        return True
+
+
+def create_board(size, mines):
+    board = Board(tuple([tuple([Cell(False) for i in range(size)])
+                         for j in range(size)]))
+    available_pos = list(range((size-1) * (size-1)))
+    for i in range(mines):
+        new_pos = random.choice(available_pos)
+        available_pos.remove(new_pos)
+        (row_id, col_id) = (new_pos % 9, new_pos // 9)
+        board.place_mine(row_id, col_id)
+    return board
+
+def get_move(board):
+    INSTRUCTIONS = ("First, enter the column, followed by the row. To add or "
+                    "remove a flag, add \"f\" after the row (for example, 64f "
+                    "would place a flag on the 6th column, 4th row). Enter "
+                    "your move: ")
+
+    move = input("Enter your move (for help enter \"H\"): ")
+    if move == "H":
+        move = input(INSTRUCTIONS)
+
+    while not is_valid(move, board):
+        move = input("Invalid input. Enter your move (for help enter \"H\"): ")
+        if move == "H":
+            move = input(INSTRUCTIONS)
+
+    return (int(move[1]), int(move[0]), True if move[-1] == "f" else False)
+
+def is_valid(move_input, board):
+    if move_input == "H" or (len(move_input) not in (2, 3) or
+                             not move_input[:1].isdigit() or
+                             int(move_input[0]) not in range(len(board)) or
+                             int(move_input[1]) not in range(len(board))):
+        return False
+
+    if len(move_input) == 3 and move_input[2] != "f":
+        return False
+
+    return True
+
+def main():
+    SIZE = 10
+    MINES = 9
+    board = create_board(SIZE, MINES)
+    print(board)
+    while board.is_playing and not board.is_solved:
+        (row_id, col_id, is_flag) = get_move(board)
+        if not is_flag:
+            board.show(row_id, col_id)
+        else:
+            board.flag(row_id, col_id)
+        print(board)
+
+    if board.is_solved:
+        print("Well done! You solved the board!")
     else:
-        nb = count_bombs_around(x,y) # Fonction à créer
-    
-        if nb == 0:
-            sense.set_pixel(x, y, white)
-        elif nb == 1:
-            sense.set_pixel(x, y, red)
-        
-        # A finir ...
+        print("Uh oh! You blew up!")
 
-# Début de game :
-new_game(5)
-# Partie qui gère le déplacement via le joystick :
-
-while game:
-    while running:
-        for event in sense.stick.get_events():         
-            if event.action == 'pressed':
-                #print(event) Pour voir sur la console les events executés.
-                
-                sense.set_pixel(x, y, old_color)
-                if event.direction == 'down' and y < 7:
-                    
-                    y = y + 1
-                    old_color =sense.get_pixel(x, y)
-                        
-                elif event.direction == 'up' and y > 0:
-                    
-                    y = y - 1
-                    old_color =sense.get_pixel(x, y)
-                        
-                elif event.direction == 'right' and x < 7:
-                    
-                    x = x + 1
-                    old_color =sense.get_pixel(x, y)
-                        
-                elif event.direction == 'left' and x > 0:
-                    
-                    x = x - 1
-                    old_color =sense.get_pixel(x, y)
-                        
-                elif event.direction == 'middle':
-                    
-                    use_case(x,y)
-                    running = False
-                    
-        if running == True:        
-            # On crée le clignotement :
-            t = int(3*time())
-            if t%2 == 0:
-                sense.set_pixel(x, y, old_color)
-            else:
-                sense.set_pixel(x, y, green)
-        
-    if running == False:
-        for event in sense.stick.get_events():
-            if event.action == 'pressed':
-                if event.direction == 'down' or event.direction == 'up' or event.direction == 'right' or event.direction == 'left':
-                    
-                    running = True
-                    
-                    if event.direction == 'down' and y < 7:
-                        
-                        y = y + 1
-                        
-                    elif event.direction == 'up' and y > 0:
-                    
-                        y = y - 1
-                        
-                    elif event.direction == 'right' and x < 7:
-                    
-                        x = x + 1
-                        
-                    elif event.direction == 'left' and x > 0:
-                    
-                        x = x - 1
+if __name__ == "__main__":
+    main()
