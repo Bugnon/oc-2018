a53d9e93a055b89a4e4d125d2ee1c5bae567410c
Raphael Holzer <raphael.holzer@citycable.ch>
2019-02-05 08:57:46 +0100

    modified games, gamelib

--- games/xxx/gamelib.py
+++ games/xxx/gamelib.py
@@ -20,6 +20,7 @@ ORANGE = (255, 128, 0)
 GRAY = (90,94,107)
 CORAIL = (231, 62, 1)
 BORDEAU = (91, 60, 19)
+LIME = (220, 255, 0)
 WHITE = (255, 255, 255)
 
 
@@ -27,7 +28,7 @@ def main():
     """Test function to display all available colors."""
     sense = SenseHat()
     colors = (BLACK, LEMON, PINK, RED, MINT, BLUE, GREEN, MAGENTA, CYAN, YELLOW,
-              ORANGE, GRAY, CORAIL, BORDEAU, WHITE)
+              ORANGE, GRAY, CORAIL, BORDEAU, LIME, WHITE)
     n = len(colors)
     print(n, 'colors')
     for x in range(8):
