8c145fdf10c141f4dba158a01d54b78a875ec73c
Valentin Piquerez <42843419+Vali186@users.noreply.github.com>
2019-03-21 13:51:34 +0100

    .

--- games2/circles/game_1.py
+++ games2/circles/game_1.py
@@ -1,13 +1,13 @@
 import pyglet
 
 # Set up a window
-x = 500
+x = 1000
 y = x*9/16
 game_window = pyglet.window.Window(x, y)
 image = pyglet.resource.image('resources/parchemin.png')
 image.height = y
 image.width = y/1.4137
-
+pyglet.gl.glClearColor(1,0,1,1)
 
 @game_window.event
 def on_draw():
