4a54855b6eb4af38fa5aeef38a8db719cd97dea9
Fab1205 <42958813+Fab1205@users.noreply.github.com>
2019-03-21 08:43:29 +0100

    Update WORDAMENT.py

--- games2/wordament/WORDAMENT.py
+++ games2/wordament/WORDAMENT.py
@@ -30,8 +30,9 @@ def on_draw():
     lines = fichier.readlines()
     for u in range(4):
         for n in range(4):
-            N_letter = pyglet.image.load('n.png')                    
-            N_letter.blit(x= pattern_size/4 * u, y=pattern_size/4 * n, height=pattern_size/4, width=pattern_size/4)
+            letter = "level/" + lines[u][n] + ".png"
+            letter_print = pyglet.image.load(letter)                        
+            letter_print.blit(x= pattern_size/4 * u, y=pattern_size/4 * n, height=pattern_size/4, width=pattern_size/4)
 
 M = [
     [0, 0, 0, 0],
