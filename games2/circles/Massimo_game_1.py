317d1e0449579a3e68e3f37f0bc48d95229c3c41
Massimo Stefani <massimo.cruz25@gmail.com>
2019-03-28 14:25:38 +0100

    ma partie

--- games2/circles/Massimo_game_1.py
+++ games2/circles/Massimo_game_1.py
@@ -1,31 +1,29 @@
 import pyglet
 from pyglet import font
+
 font.add_file('resources/Handwriting.ttf')
 Handwriting = font.load('Janda Elegant Handwriting', 14)
 
 # Set up a window
 rapportparchemin = 3506/2480
-x = 500
+x = 800
 y = x*9/16
 game_window = pyglet.window.Window(x, y)
 image = pyglet.resource.image('resources/parchemin.png')
 image.height = y
 image.width = y/rapportparchemin
 
-
 def write_poetry():
-    num_lines = 0
-    with open("resources/poeme.txt", "r") as f:
-        for line in f:
-            num_lines += 1
-    print("Number of lines:")
-    print(num_lines)
     txt = open("resources/poeme.txt", "r")
-    poetry = pyglet.text.Label(str(txt.read()),
-                          font_name='Janda Elegant Handwriting',
-                          font_size=8,
-                          x=x*9/16, y=100)
-    poetry.draw()
+    saut = 0
+    for line in txt:
+        saut = saut+15
+        poeme = pyglet.text.Label(line,
+                            font_name='Arial',
+                            font_size=9,
+                            color=(75, 0, 130, 255),
+                            x=x-300, y=y-30-saut)
+        poeme.draw()
 
 @game_window.event
 def on_draw():
