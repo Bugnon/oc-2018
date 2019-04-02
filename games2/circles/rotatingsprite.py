36382ae704aa4beede7a991a44e823ffd9d6407e
Valentin Piquerez <42843419+Vali186@users.noreply.github.com>
2019-04-02 09:11:09 +0200

    .

--- games2/circles/rotatingsprite.py
+++ games2/circles/rotatingsprite.py
@@ -1,6 +1,8 @@
 import pyglet
 import math
+from pyglet.window import key
 
+player_image = pyglet.resource.image("resources/encrier.png")
 # définition d'une nouvelle classe
 class RotatingSprite(pyglet.sprite.Sprite):
     def __init__(self, *args, **kwargs):
@@ -21,3 +23,31 @@ class RotatingSprite(pyglet.sprite.Sprite):
     def update(self, dt):
         self.angle += self.angular_velocity * dt
         self.update_position()
+
+class Player(pyglet.sprite.Sprite):
+    def __init__(self, *args, **kwargs):
+        super(Player, self).__init__(img=player_image, *args, **kwargs)
+        
+        self.rotate_speed=150
+
+        self.keys = dict(left=False, true=False)
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.LEFT:
+            self.keys['left'] = True
+        elif symbol == key.RIGHT:
+            self.keys['right'] = True
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == key.LEFT:
+            self.keys['left'] = False
+        elif symbol == key.RIGHT:
+            self.keys['right'] = False
+    
+    def update(self, dt):
+        super(Player, self).update(dt)
+
+        if self.keys['left']:
+            self.rotation -= self.rotate_speed * dt
+        elif self.keys['right']:
+            self.rotation += self.rotate_speed * dt
